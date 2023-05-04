40a08451f4e46cf0e70509855c4967ec1e1e2279
Yash Shrivastava <yash.shrivastava77@gmail.com>
2023-05-04 18:11:57 +0530

    Implement basic API with stubbing

--- server.py
+++ server.py
@@ -1,7 +1,17 @@
-import fastapi from FastAPI
+from fastapi import FastAPI
 
 app = FastAPI()
 
+reviews = ["Hello" , "World!"]
 @app.get("/")
 def root():
     return {"Server running!"}
+
+@app.post("/reviews")
+def addReviews(review_data):
+    reviews.append(review_data)
+    return {"message" : "Review added successfully"}
+
+@app.get("/reviews")
+def getReviews():
+    return reviews
